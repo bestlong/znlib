9f83f2d25833105c868d08d87b13a5933ba83b53
dmznlin <dmznlin@83027714-c433-f0c1-626c-e468a2579b09>
2010-11-16 04:19:25 +0000

    *.添加微秒级计数器.

--- trunk/LibFun/UWaitItem.pas
+++ trunk/LibFun/UWaitItem.pas
@@ -1,21 +1,18 @@
 {*******************************************************************************
   作者: dmzn@163.com 2007-11-23
-  描述: 实现一个等待对象
+  描述: 实现等待对象和高性能等待计数器
 
   描述:
   &.TWaitObject在EnterWait后进入阻塞,直到Wakeup唤醒.
   &.该对象多线程安全,即A线程EnterWait,B线程Wakeup.
-
-  *.注意:
-  &.方法EnterWait与Wakeup成对出现,即使多线程操作,也需要先EnterWait->Wakeup.
-    对于EnterWait->EnterWait,Wakeup->Wakeup是不成功的.
+  &.TWaitTimer实现微秒级间隔计数.
 *******************************************************************************}
 unit UWaitItem;
 
 interface
 
 uses
-  Windows, Classes;
+  Windows, Classes, SysUtils;
 
 type
   TWaitObject = class(TObject)
@@ -39,6 +36,29 @@ type
     property Interval: Cardinal read FInterval write FInterval;
   end;
 
+  TWaitTimer = class(TObject)
+  private
+    FFrequency: Int64;
+    {*CPU频率*}
+    FFlagFirst: Int64;
+    {*起始标记*}
+    FTimeResult: Int64;
+    {*计时结果*}
+  public
+    constructor Create;
+    procedure StartTime;
+    {*开始计时*}
+    function EndTime: Int64;
+    {*结束计时*}
+    property TimeResult: Int64 read FTimeResult;
+    {*属性相关*}
+  end;
+
+procedure StartHighResolutionTimer;
+//开始计数
+function GetHighResolutionTimerResult: Int64;
+//获取微秒计数结果
+
 implementation
 
 const
@@ -61,13 +81,10 @@ begin
 end;
 
 function TWaitObject.EnterWait: Cardinal;
-var nInt: Integer;
 begin
-  nInt := InterlockedExchange(FStatus, cIsWaiting);
-  if nInt = cIsWaiting then Exit;
-  //is waiting
-
+  InterlockedExchange(FStatus, cIsWaiting);
   Result := WaitForSingleObject(FEvent, FInterval);
+
   FWaitResult := Result;    
   InterlockedExchange(FStatus, cIsIdle);
 end;
@@ -76,7 +93,54 @@ procedure TWaitObject.Wakeup;
 begin
   if FStatus = cIsWaiting then
     SetEvent(FEvent);
-  //do this only waiting
+  //do only waiting
 end;
 
+//------------------------------------------------------------------------------
+constructor TWaitTimer.Create;
+begin
+  FTimeResult := 0;
+  if not QueryPerformanceFrequency(FFrequency) then
+    raise Exception.Create('not support high-resolution performance counter');
+  //xxxxx
+end;
+
+procedure TWaitTimer.StartTime;
+begin
+  QueryPerformanceCounter(FFlagFirst);
+end;
+
+function TWaitTimer.EndTime: Int64;
+var nNow: Int64;
+begin
+  QueryPerformanceCounter(nNow);
+  Result := Trunc((nNow - FFlagFirst) / FFrequency * 1000 * 1000);
+  FTimeResult := Result;
+end;
+
+//------------------------------------------------------------------------------
+var
+  gTimer: TWaitTimer = nil;
+  //高性能计数器
+
+//Desc: 开始一个计数
+procedure StartHighResolutionTimer;
+begin
+  if not Assigned(gTimer) then
+    gTimer := TWaitTimer.Create;
+  gTimer.StartTime;
+end;
+
+//Desc: 返回计数结果
+function GetHighResolutionTimerResult: Int64;
+begin
+  if Assigned(gTimer) then
+       Result := gTimer.EndTime
+  else Result := 0;
+end;
+
+initialization
+
+finalization
+  FreeAndNil(gTimer);
 end.
